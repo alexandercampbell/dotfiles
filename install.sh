700d13892f31aaa16f68c46ffa2f86878cb3c2a4
Alexander Campbell <alexanderhcampbell@gmail.com>
2015-01-16 12:35:03 -0600

    Switch to Zsh, add zshrc

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 
+# Install Zsh
+curl -L http://install.ohmyz.sh | sh
+
 # Install Vundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # Set up symbolic links to configuration files
-ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/vimrc ~/.vimrc
-ln -s ~/dotfiles/bash_profile ~/.bash_profile
+ln -s ~/dotfiles/tmux.conf      ~/.tmux.conf
+ln -s ~/dotfiles/vimrc          ~/.vimrc
+ln -s ~/dotfiles/bash_profile   ~/.bash_profile
+ln -s ~/dotfiles/zshrc          ~/.zshrc
 
 

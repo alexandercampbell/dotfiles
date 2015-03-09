33925d7e4e575abd6d326896fceadac376ee62d6
Alexander Campbell <alexanderhcampbell@gmail.com>
2015-03-09 12:58:44 -0500

    Reorder installation steps in install.sh

--- install.sh
+++ install.sh
@@ -2,16 +2,16 @@
 # Get required packages
 sudo apt-get install -y zsh bash vim git mercurial tmux curl
 
-# Install Oh-My-Zsh
-curl -L http://install.ohmyz.sh | sh
-
-# Install Vundle
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
 # Set up symbolic links to configuration files
 ln -s ~/dotfiles/tmux.conf      ~/.tmux.conf
 ln -s ~/dotfiles/vimrc          ~/.vimrc
 ln -s ~/dotfiles/bash_profile   ~/.bash_profile
 ln -s ~/dotfiles/zshrc          ~/.zshrc
+
+# Install Oh-My-Zsh
+curl -L http://install.ohmyz.sh | sh
+
+# Install Vundle
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 

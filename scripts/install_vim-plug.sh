636b7c5739e664809ab70e71dc5aa55bc6addd29
Alexander Campbell <alexanderhcampbell@gmail.com>
2019-05-13 17:47:19 -0500

    scripts: update

--- scripts/install_vim-plug.sh
+++ scripts/install_vim-plug.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-# Install for Neovim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# Symlink that file so that it will also work for regular Vim (and GVim).
-mkdir -p ~/.vim/autoload/
-ln -s ~/.local/share/nvim/site/autoload/plug.vim ~/.vim/autoload/plug.vim

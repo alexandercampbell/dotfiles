e24278048abc05a5f58620bfb3f74ea4d29085c9
Alexander Campbell <alexanderhcampbell@gmail.com>
2020-06-04 18:38:19 -0600

    scripts/nrepl: update to latest cider-nrepl

--- scripts/nrepl.sh
+++ scripts/nrepl.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
 
-clj -Sdeps '{:deps {cider/cider-nrepl {:mvn/version "0.22.4"} }}' \
+clj -Sdeps '{:deps {cider/cider-nrepl {:mvn/version "0.25.1"} }}' \
 	-m nrepl.cmdline --middleware "[cider.nrepl/cider-middleware]"
 

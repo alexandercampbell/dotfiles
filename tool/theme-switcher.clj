4a0ba3db334cc5dc7c1c367e4c83a1f16b318d2c
Alexander Campbell <alexanderhcampbell@gmail.com>
2024-03-12 20:49:53 -0500

    theme-switcher: simplify

--- tool/theme-switcher.clj
+++ tool/theme-switcher.clj
@@ -5,37 +5,32 @@
 (def home (System/getenv "HOME"))
 (def os-dark (-> (System/getenv "DARK_MODE") (= "1")))
 
-; NOTE: regexes are expected to have three groups. The middle group ($2) will
-; be replaced with the new value.
-
-(def alacritty
-  {:path  (str home "/.config/alacritty/alacritty.toml")
-   :regex #"(alacritty-theme/themes/)([^\".]+)(.toml\")"
-   :dark  "github_dark_dimmed"
-   :light "ashes_light"})
-
-(def helix
-  {:path  (str home "/.config/helix/config.toml")
-   :regex #"(theme = \")([^\"]+)(\")"
-   :dark  "modus_vivendi"
-   :light "modus_operandi_tinted"})
-
-(def zellij
-  {:path  (str home "/.config/zellij/config.kdl")
-   :regex #"(theme \")([^\"]+)(\")"
-   :dark  "terafox"
-   :light "dayfox"})
-
 (defn replace-in-file
   [path pattern replacement]
   (let [original (slurp path)
         updated  (str/replace-first original pattern replacement)]
     (spit path updated)))
 
-(defn handle-program
-  [program]
-  (let [{:keys [path regex dark light]} program
+(def program-configs
+  ; NOTE: regexes are expected to have three groups. The middle group ($2)
+  ; will be replaced with the new value.
+  {:alacritty {:path  (str home "/.config/alacritty/alacritty.toml")
+               :regex #"(alacritty-theme/themes/)([^\".]+)(.toml\")"
+               :dark  "github_dark_dimmed"
+               :light "ashes_light"}
+   :helix     {:path  (str home "/.config/helix/config.toml")
+               :regex #"(theme = \")([^\"]+)(\")"
+               :dark  "modus_vivendi"
+               :light "modus_operandi_tinted"}
+   :zellij    {:path  (str home "/.config/zellij/config.kdl")
+               :regex #"(theme \")([^\"]+)(\")"
+               :dark  "terafox"
+               :light "dayfox"}})
+
+(defn update-program-config
+  [program-config]
+  (let [{:keys [path regex dark light]} program-config
         replacement                     (str "$1" (if os-dark dark light) "$3")]
     (replace-in-file path regex replacement)))
 
-(dorun (map handle-program [alacritty helix zellij]))
+(->> program-configs vals (map update-program-config) dorun)
